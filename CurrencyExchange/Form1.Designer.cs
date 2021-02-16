237f46323ac2ed895bd67edee230509f17000bf3
Вадим Бабиков <b.vadim.alexeyevich@gmail.com>
2021-02-16 17:52:49 +0700

    ready

---
+++ CurrencyExchange/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace CurrencyExchange
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button_calculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numeric_initial_price = new System.Windows.Forms.NumericUpDown();
+            this.numeric_days = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_initial_price)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_days)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea1.AxisY.IsStartedFromZero = false;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 79);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Color = System.Drawing.Color.Teal;
+            series1.IsValueShownAsLabel = true;
+            series1.LabelFormat = "f4";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(776, 359);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // button_calculate
+            // 
+            this.button_calculate.Location = new System.Drawing.Point(599, 35);
+            this.button_calculate.Name = "button_calculate";
+            this.button_calculate.Size = new System.Drawing.Size(75, 23);
+            this.button_calculate.TabIndex = 1;
+            this.button_calculate.Text = "Calculate";
+            this.button_calculate.UseVisualStyleBackColor = true;
+            this.button_calculate.Click += new System.EventHandler(this.button_calculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(56, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Initial price:";
+            // 
+            // numeric_initial_price
+            // 
+            this.numeric_initial_price.Location = new System.Drawing.Point(141, 35);
+            this.numeric_initial_price.Name = "numeric_initial_price";
+            this.numeric_initial_price.Size = new System.Drawing.Size(120, 22);
+            this.numeric_initial_price.TabIndex = 3;
+            // 
+            // numeric_days
+            // 
+            this.numeric_days.Location = new System.Drawing.Point(387, 33);
+            this.numeric_days.Name = "numeric_days";
+            this.numeric_days.Size = new System.Drawing.Size(120, 22);
+            this.numeric_days.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(337, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Days:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numeric_days);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numeric_initial_price);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_calculate);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_initial_price)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numeric_days)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button_calculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numeric_initial_price;
+        private System.Windows.Forms.NumericUpDown numeric_days;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CurrencyExchange/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CurrencyExchange
+{
+    public partial class Form1 : Form
+    {
+        const double k = 0.02;
+        Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_calculate_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+            double price = (double)numeric_initial_price.Value;
+            int days = (int)numeric_days.Value;
+            chart1.Series[0].Points.AddXY(0, price);
+            for (int i = 1; i <= days; i++)
+            {
+                price = price * (1 + k * (rnd.NextDouble() - 0.5));
+                chart1.Series[0].Points.AddXY(i, price);
+            }
+        }
+    }
+}
